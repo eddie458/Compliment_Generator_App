f992619e3d371e8377aba9615e1455e8433f08cf
Eddie <jesusnunez8517@outlook.com>
2025-09-25 11:44:11 -0600

    Added all the files currently used

---
+++ Compliment_Generator_App/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        buttonGenerate = New Button()
+        textbox_Name = New TextBox()
+        labelName = New Label()
+        listboxCategories = New ListBox()
+        labelCategory = New Label()
+        SuspendLayout()
+        ' 
+        ' buttonGenerate
+        ' 
+        buttonGenerate.BackColor = Color.Orchid
+        buttonGenerate.Font = New Font("Century Gothic", 10.2F, FontStyle.Bold Or FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        buttonGenerate.ForeColor = Color.Purple
+        buttonGenerate.Location = New Point(12, 12)
+        buttonGenerate.Name = "buttonGenerate"
+        buttonGenerate.Size = New Size(234, 60)
+        buttonGenerate.TabIndex = 0
+        buttonGenerate.Text = "Give me a compliment!"
+        buttonGenerate.UseVisualStyleBackColor = False
+        ' 
+        ' textbox_Name
+        ' 
+        textbox_Name.Font = New Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        textbox_Name.Location = New Point(108, 81)
+        textbox_Name.Name = "textbox_Name"
+        textbox_Name.Size = New Size(138, 26)
+        textbox_Name.TabIndex = 1
+        ' 
+        ' labelName
+        ' 
+        labelName.AutoSize = True
+        labelName.Font = New Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        labelName.Location = New Point(12, 84)
+        labelName.Name = "labelName"
+        labelName.Size = New Size(90, 20)
+        labelName.TabIndex = 2
+        labelName.Text = "Your name:"
+        ' 
+        ' listboxCategories
+        ' 
+        listboxCategories.Font = New Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        listboxCategories.FormattingEnabled = True
+        listboxCategories.Items.AddRange(New Object() {"Confidence-Boosting", "Gentle Encouragement", "Fun & Silly"})
+        listboxCategories.Location = New Point(12, 152)
+        listboxCategories.Name = "listboxCategories"
+        listboxCategories.Size = New Size(234, 64)
+        listboxCategories.TabIndex = 3
+        ' 
+        ' labelCategory
+        ' 
+        labelCategory.AutoSize = True
+        labelCategory.Font = New Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        labelCategory.Location = New Point(12, 120)
+        labelCategory.Name = "labelCategory"
+        labelCategory.Size = New Size(234, 20)
+        labelCategory.TabIndex = 4
+        labelCategory.Text = "Desired Compliment Category:"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LavenderBlush
+        ClientSize = New Size(265, 233)
+        Controls.Add(labelCategory)
+        Controls.Add(listboxCategories)
+        Controls.Add(labelName)
+        Controls.Add(textbox_Name)
+        Controls.Add(buttonGenerate)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Compliment Generator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents buttonGenerate As Button
+    Friend WithEvents textbox_Name As TextBox
+    Friend WithEvents labelName As Label
+    Friend WithEvents listboxCategories As ListBox
+    Friend WithEvents labelCategory As Label
+
+End Class
---
+++ Compliment_Generator_App/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub buttonGenerate_Click(sender As Object, e As EventArgs) Handles buttonGenerate.Click
+
+        ' Remember that you can just put the name of your file in the path if it is where the .exe is generated
+        ' Variable used to store the MS Access database file path
+        Dim dbPath As String = "ComplimentDB.accdb"
+
+        ' Variable that stores the database provider version and the dbPath
+        ' Remember to download AccessDatabaseEngine_X64.exe from the web here: Download Microsoft Access Database Engine 2016 Redistributable
+        Dim connDBString As String = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={dbPath};"
+
+        ' Variable for storing the name of the current user
+        Dim nameUser As String = textbox_Name.Text
+
+        Dim selectedCategory As String = listboxCategories.SelectedItem
+
+        Using connDB As New OleDbConnection(connDBString)
+            Try
+                ' Open a connection to the database
+                connDB.Open()
+                ' MessageBox.Show("Connected to Access Database!") ' You can also use this command: MsgBox("Connected to Access Database!")
+
+                ' SQL query used for selecting 1 random compliment from the Compliments table
+                ' SELECT TOP # gets the number of records specified from the Compliments table, and orders it randomly
+                Dim sqlQuery1 As String
+
+                If (selectedCategory = "Confidence-Boosting") Then
+                    sqlQuery1 = "SELECT TOP 1 ComplimentText FROM Compliments WHERE CategoryID = 1 ORDER BY Rnd(-Timer() * ComplimentID)"
+                ElseIf (selectedCategory = "Gentle Encouragement") Then
+                    sqlQuery1 = "SELECT TOP 1 ComplimentText FROM Compliments WHERE CategoryID = 2 ORDER BY Rnd(-Timer() * ComplimentID)"
+                ElseIf (selectedCategory = "Fun & Silly") Then
+                    sqlQuery1 = "SELECT TOP 1 ComplimentText FROM Compliments WHERE CategoryID = 3 ORDER BY Rnd(-Timer() * ComplimentID)"
+                Else
+                    sqlQuery1 = "SELECT TOP 1 ComplimentText FROM Compliments ORDER BY Rnd(-Timer() * ComplimentID)" '"SELECT TOP 1 ComplimentText FROM Compliments ORDER BY Rnd(-Timer() * ComplimentID)"
+                End If
+
+                ' Create a db command with the connection object and the sql query
+                Using cmd1 As New OleDbCommand(sqlQuery1, connDB)
+
+                    ' Executes the sql query command and returns the result
+                    Dim compliment As Object = cmd1.ExecuteScalar()
+
+                    If compliment IsNot Nothing Then
+
+                        ' Variable used for storing replaced name compliment
+                        Dim complimentTextReplaced As String
+
+                        ' Convert compliment to string and the replace [Name] with the actual name of the user
+                        complimentTextReplaced = compliment.ToString().Replace("[Name]", $"{nameUser}")
+
+                        MessageBox.Show("Your compliment of the day is: " & Environment.NewLine & complimentTextReplaced)
+
+                    Else
+                        MessageBox.Show("No compliments found in database.")
+                    End If
+
+                End Using
+
+                If connDB.State = ConnectionState.Open Then connDB.Close()
+
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+
+            End Try
+
+        End Using
+        ' Example comment for GIT reference
+
+    End Sub
+End Class
